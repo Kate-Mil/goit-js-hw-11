bbd84431dfd60185cda92638d32dab9baa8448b5
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-04-03 15:37:15 +0300

    made throught async function

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const API_KEY = '34900883-94108098f0e7f8bd03a1699df';
 const BASE_URL = 'https://pixabay.com/api/';
 const ADD_API_REQUEST = 'image_type=photo&orientation=horizontal&safesearch=true';
@@ -11,20 +13,34 @@ constructor (){
     this.per_page = 40;
 }
 
-fetchPictures(){
-return fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${ADD_API_REQUEST}&per_page=${this.per_page}&page=${this.page}`)
-.then(res => {
-    if (!res.ok) {
-        throw new Error(res.status);
-    }
-    return res.json();
-    })
-    .then(data => {
+async fetchPictures(){
+    try {
+        return await axios.get(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${ADD_API_REQUEST}&per_page=${this.per_page}&page=${this.page}`)
+        .then(({data:{hits, totalHits}}) => {
         this.incrementPage();
-        return data;
-       
+        return {hits, totalHits};
     })
+        } catch (respons) {
+        console.log('an error occurred, please try again later');
+     }
+    
 }
+
+
+// fetchPictures(){
+// return fetch(`${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${ADD_API_REQUEST}&per_page=${this.per_page}&page=${this.page}`)
+// .then(res => {
+//     if (!res.ok) {
+//         throw new Error(res.status);
+//     }
+//     return res.json();
+//     })
+//     .then(data => {
+//         this.incrementPage();
+//         return data;
+       
+//     })
+// }
 
 incrementPage() {
 this.page +=1;
